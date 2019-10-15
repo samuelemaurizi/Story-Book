21e67a0d8523b4afcac443ee810d13794f63bfd1
Samuele Maurizi <samuelemaurizi@gmail.com>
2019-10-15 17:46:28 +0200

    managed auth

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('dotenv').config();
+require('./config/passport')(passport);
+const auth = require('./routes/auth');
 
 const app = express();
 
+// Routes
 app.get('/', (req, res) => {
   res.send('Connected!');
 });
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started om port ${port}...`));
---
+++ config/passport.js
@@ -0,0 +1,27 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const keys = require('./keys');
+
+module.exports = function(passport) {
+  passport.use(
+    new GoogleStrategy(
+      // {
+      //   clientID: keys.googleClientID,
+      //   clientSecret: keys.googleClientSecret,
+      //   callbackURL: '/auth/google/callback',
+      //   proxy: true
+      // },
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+      },
+      (accessToken, refrechToken, profile, done) => {
+        console.log(accessToken);
+        console.log(profile);
+      }
+    )
+  );
+};
